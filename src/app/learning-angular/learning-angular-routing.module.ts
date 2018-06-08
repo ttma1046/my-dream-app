65b9c712a9728690f13576f274123b90e937fbc0
Chandler Fang <chandler.fang@cashrewards.com>
2018-06-08 14:56:58 +1000

    Fix typo.

--- src/app/learning-angular/learning-angular-routing.module.ts
+++ src/app/learning-angular/learning-angular-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { StartlearnComponent } from './components/startlearn/startlearn.component';
 import { LetslearnComponent } from './components/letslearn/letslearn.component';
 const learnAngularRoutes: Routes = [
-   { path: 'startlearn', component: StartlearnComponent }
+   { path: 'startlearn', component: StartlearnComponent },
    { path: 'letslearn', component: LetslearnComponent }
 ];
 
